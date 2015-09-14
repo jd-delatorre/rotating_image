40317b3f3ad9a6fb2096fec4d81b8ba68870a7fa
jdelatorre312 <jdelatorre312@gmail.com>
2015-09-13 20:47:13 -0400

    Removing duplicate index and script files.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,292 +1,83 @@
 /**
- * Created by jdelatorre312 on 9/12/15.
+ * Created by jdelatorre312 on 9/13/15.
  */
 
-$(function() {
-//our 4 items
+$(function(){
 
-var $listItems = $('#rm_container > ul > li'),
-    totalItems = $listItems.length,
+    //selecting the li for the main images.
+    var $imageselection = $('#rm_container').find('li');
 
-    //the controls
+    //for initial setup of images, getting the rotation degree from the data attribute
+    $.each($imageselection, function(i, val){
 
-    $rm_next = $('#rm_next'),
-    $rm_prev = $('#rm_prev'),
-    $rm_play = $('#rm_play'),
-    $rm_pause = $('#rm_pause'),
+        var rotation = $(val).data('rotation');
+        $(val).animate({
+            transform:'rotate('+ rotation + 'deg)'
+        });
 
-    //the masks and corners of the sliders
+    });
 
-    $rm_mask_left = $('#rm_mask_left'),
-    $rm_mask_right = $('#rm_mask_right'),
-    $rm_corner_left = $('#rm_corner_left'),
-    $rm_corner_right = $('#rm_corner_right'),
+    //binding the rotating image initialization to the left and right arrows.
+    $('#rm_next').on('click', function(){
+        rotateImage(-1);
+    });
 
-//check if the browser is <= IE8
-    ieLte8			= ($.browser.msie && parseInt($.browser.version) <= 8),
+    $('#rm_prev').on('click', function(){
+        rotateImage(1);
+    });
 
+    //rotating image function
+    function rotateImage(direction){
+        $.each($imageselection, function(i, val){
 
-    RotateImageMenu = (function(){
+            var $item = $(this);
 
-        //difference of animation time between the items
-        var timeDiff = 300,
+            //settimeout so that there is a delay after clicking
+            setTimeout(function(){
 
-            //time between each animation
-            slideshowTime = 3000,
-            slideshowInterval,
+                var $otherImages = $('#' + $item.data('images')).children('img'),
+                    totalOtherImages = $otherImages.length;
 
-            //checks if images are rotating
-            isRotating = false,
+                    $img = $item.children('img:last'),
+                    current = $item.data('current');
 
-            //how many images completed each slideshow iteration
-            completed = 0,
+                //if current counter is more than count of images, reset
+                if( current > totalOtherImages - 1){
+                    current = 0;
+                }else if( current < 0){
+                    current = totalOtherImages - 1;
+                }else{}
 
-            /*
-            * all our images are 310 width and 465 height, this could/should be dynamically
-            * calculated if we had different size images.
-            *
-            * we will set the rotational origin at x = width/2 and y = height*2
-            *
-            * */
+                var $other = $otherImages.eq(current).clone();
 
-            origin = ['155px', '930px'],
-            init = function(){
-                configure();
-                initEventsHandler();
-            },
+                //depending on direction, next or previous, set degree
+                var rotatedirection = (direction === 1) ? '30deg' : '-30deg';
 
-            //initialize some events
-            initEventsHandler = function(){
-                /*
-                * next and previous arrows
-                * we will stop slideshow if active
-                * and rotate each image
-                *
-                * 1 for rotate right
-                * -1 for rotate left
-                *
-                * */
+                //setting the transform origin and the rotation
+                $other.css('transform', 'rotate(' + rotatedirection + ')');
+                $other.css('transform-origin', '155px 930px 0px');
+                $img.css('transform-origin', '155px 930px 0px');
 
-                $rm_next.bind('click', function(e){
-                    stopSlideshow();
-                    rotateImages(1);
-                    return false;
+                (direction ===1) ? ++current : --current;
+
+                $item.data('current', current).prepend($other);
+
+                var rotateTo = (direction === 1) ? '-80deg' : '80deg';
+
+                //animation that gives the effect of the rotation.
+                $img.animate({
+                    transform:'rotate('+ rotateTo +')'
+                }, 1200, function(){
+                    $(this).remove();
                 });
 
-                $rm_prev.bind('click', function(e){
-                    stopSlideshow();
-                    rotateImages(-1);
-                    return false;
-                });
+                $other.animate({
+                    transform:'rotate(0deg)'
+                }, 600);
 
-                //start and stop slideshow
+            }, 300);
 
-                $rm_play.bind('click', function(e){
-                    startSlideshow();
-                    return false;
-                });
+        });
+    };
 
-                $rm_pause.bind('click', function(e){
-                    stopSlideshow();
-                    return false;
-                });
-
-                //add events to mouse and left and right keys
-
-                $(document).bind('mousewheel', function(e, delta){
-
-                    if(delta > 0){
-                        stopSlideshow();
-                        rotateImages(0);
-                    }else{
-                        stopSlideshow();
-                        rotateImages(1);
-                    }
-
-                    return false;
-
-                }).keydown(function(e){
-                    switch (e.which){
-                        case 37:
-                            stopSlideshow();
-                            rotateImages(0);
-                            break;
-                        case 39:
-                            stopSlideshow();
-                            rotateImages(1);
-                            break;
-                    }
-                });
-            },
-
-            //rotates each item's images
-
-            rotateImages = function(dir){
-                //if animation is in progress return
-                if(isRotating) return false;
-
-                isRotating = true;
-
-                $listItems.each(function(i){
-                    var $item = $(this),
-                    /*
-                    * the delay calculation.
-                    * if rotation is to the right, then the first item to rotate is the first one
-                    * otherwise the last one.
-                    */
-
-                        interval = (dir === 1) ? i * timeDiff: (totalItems - 1 - i) * timeDiff;
-
-                    setTimeout(function(){
-                        var $otherImages = $('#' + $item.data('images')).children('img'),
-                            totalOtherImages = $otherImages.length;
-
-                        //the current one
-                        $img = $item.children('img:last'),
-                            //keep track of each items current image
-                        current = $item.data('current');
-
-                        //out of bounds
-                        if(current > totalOtherImages - 1){
-                            current = 0;
-                        }else if(current < 0){
-                            current = totalOtherImages - 1
-                        }else{}
-
-                        var otherRotation	= (dir === 1) ? '-30deg' : '30deg',
-                            $other			= $otherImages.eq(current).clone();
-
-                        //for IE <= 8 we will not rotate, but fade out / fade in ...
-                        //better than nothing :)
-                        if(!ieLte8)
-                            $other.css({
-                                rotate	: otherRotation,
-                                origin	: origin
-                            });
-
-                        (dir === 1) ? ++current : --current;
-
-                        //prepend the next image to the li
-
-                        $item.data('current', current).prepend($other);
-
-                        //the final rotation for the current image
-
-                        var rotateTo = (dir === 1) ? '80deg' : '-80deg';
-
-                        if(!ieLte8) {
-                            $img.animate({
-                                rotate	: rotateTo
-                            }, 1200, function(){
-                                $(this).remove();
-                                ++completed;
-                                if(completed === 4) {
-                                    completed = 0;
-                                    isRotating = false;
-                                }
-                            });
-                            $other.animate({
-                                rotate	: '0deg'
-                            }, 600);
-                        }
-                        else {
-                            $img.fadeOut(1200, function(){
-                                $(this).remove();
-                                ++completed;
-                                if(completed === 4) {
-                                    completed = 0;
-                                    isRotating = false;
-                                }
-                            });
-                        }
-                    }, interval);
-                });
-            },
-
-            //set initial rotations
-
-            configure = function(){
-                if($.browser.msie && !ieLte8)
-                    rotateMaskCorners();
-                else if(ieLte8)
-                    hideMaskCorners();
-
-                $listItems.each(function(i) {
-                    //the initial current is 1
-                    //since we already showing the first image
-                    var $item = $(this).data('current', 1);
-
-                    if(!ieLte8)
-                        $item.transform({rotate: $item.data('rotation') + 'deg'})
-                            .find('img')
-                            .transform({origin: origin});
-                });
-            },
-
-            //rotates mask and corners
-            rotateMaskCorners = function(){
-                $rm_mask_left.transform({rotate : '-3deg'});
-                $rm_mask_right.transform({ rotate : '3deg'});
-                $rm_corner_left.transform({ rotate : '45deg'});
-                $rm_corner_right.transform({ rotate: '-45deg'});
-            },
-            //hide the mask and corners
-            hideMaskCorners = function(){
-                $rm_mask_left.hide();
-                $rm_mask_right.hide();
-                $rm_corner_left.hide();
-                $rm_corner_right.hide();
-            },
-            startSlideshow = function(){
-                clearInterval(slideshowInterval);
-                rotateImages(1);
-                slideshowInterval = setInterval(function(){
-                    rotateImages(1);
-                }, slideshowTime );
-
-                //show the pause button and hide the play button
-                $rm_play.hide();
-                $rm_pause.show();
-            },
-            stopSlideshow = function(){
-                clearInterval(slideshowInterval);
-                //show the play button and hide the pause button
-                $rm_play.show();
-                $rm_pause.hide();
-            };
-
-        return {init : init};
-
-    })();
-
-    RotateImageMenu.init();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
